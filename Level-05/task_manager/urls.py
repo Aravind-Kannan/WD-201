75686ed9a7bd26cdce5bd282b842cf40d7a75f12
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-01-28 10:36:07 +0300

    level 5 notes: added comment to urls.py

--- Level-05/task_manager/urls.py
+++ Level-05/task_manager/urls.py
@@ -27,8 +27,8 @@ from tasks.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", tasks_view),
-    # add-task appends query params after ? => so add-task/?... gets redirect to add add-task?...
-    # GET /add-task?task=Get+some+veggies
+    # /add-task appends query params after ? => so add-task?... gets redirect to add add-task/?...
+    # GET /add-task/?task=Get+some+veggies
     path("add-task/", add_task_view),
     path("delete-task/<int:index>/", delete_task_view),
     path("complete_task/<int:index>/", complete_task_view),
